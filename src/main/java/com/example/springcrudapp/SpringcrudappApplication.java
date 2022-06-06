42e4c186bb7dcd7d319dcc1af20a7b61ae543d96
Yusuf Akin <kutluhanakin@gmail.com>
2022-06-06 15:56:32 -0400

    Initial commit

---
+++ src/main/java/com/example/springcrudapp/SpringcrudappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcrudappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcrudappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcrudapp/SpringcrudappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcrudappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
